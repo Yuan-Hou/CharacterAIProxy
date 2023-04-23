cda94245824b5f9a7c88d3693b023fa6a826ab9d
Yuan-Hou <lys138389@126.com>
2023-04-23 22:02:11 +0800

    验证一下可行性

--- api/hello.ts
+++ api/hello.ts
@@ -1,8 +1,20 @@
 import type { VercelRequest, VercelResponse } from '@vercel/node'
+import CharacterAI from 'node_characterai';
+const characterAI = new CharacterAI();
 
-export default function handler(req: VercelRequest, res: VercelResponse) {
+export default async function handler(req: VercelRequest, res: VercelResponse) {
   const { name = 'World' } = req.query
+  await characterAI.authenticateAsGuest();
+
+  const characterId = "8_1NyR8w1dOXmI1uWaieQcd147hecbdIK7CeEAIrdJw" // Discord moderator
+
+  const chat = await characterAI.createOrContinueChat(characterId);
+  const response = await chat.sendAndAwaitResponse('Hello discord mod!', true)
+
+  console.log(response);
+
   return res.json({
     message: `Hello ${name}!`,
+    resp: response
   })
 }
